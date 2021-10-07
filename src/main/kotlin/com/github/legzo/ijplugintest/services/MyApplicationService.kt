41fd2854aac9dfe6a6dbbb88c860bc0afd26690f
GitHub Action <action@github.com>
2021-10-07 09:06:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/legzo/ijplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/legzo/ijplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.legzo.ijplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.legzo.ijplugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/legzo/ijplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/legzo/ijplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.legzo.ijplugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.legzo.ijplugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
